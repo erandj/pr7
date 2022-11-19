ce3f90f2f34981d0aab01953502283e5a96597bc
Степанов Эллэй <stepanovelley@iMac-Mac005-3.local>
2022-11-19 09:23:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pr7/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.pr7.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.pr7.FirstActivity;
+import com.example.pr7.R;
+import com.example.pr7.SecondActivity;
+import com.example.pr7.ThirdActivity;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                FirstActivity tab1 = new FirstActivity();
+                return tab1;
+            case 1:
+                SecondActivity tab2 = new SecondActivity();
+                return tab2;
+            case 2:
+                ThirdActivity tab3 = new ThirdActivity();
+                return tab3;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
